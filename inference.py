94b74027ea4c4b85e0d4c98c073756c3f9b5ec71
Aaricis <947165603@qq.com>
2026-04-02 15:29:56 +0800

    feat: inference

---
+++ inference.py
@@ -0,0 +1,97 @@
+import argparse
+import json
+import os
+
+import torch
+import tqdm
+from peft import PeftModel
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+from utils import get_bnb_config, get_prompt_with_template
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Inferencing using a model."
+    )
+    parser.add_argument("--model_path", type=str, default="Qwen/Qwen3-4B", help="Model name or path.")
+    parser.add_argument("--adapter_checkpoint_path", type=str, default="Qwen/Qwen3-4B",
+                        help="Path to the adapter checkpoint.")
+    parser.add_argument("--test_data_path", type=str, default="data/public_test.json", help="Path to the test dataset.")
+    parser.add_argument("--output_path", type=str, default="output.json", help="Path to save the output.")
+    parser.add_argument("--batch_size", type=int, default=8, help="Batch size for inference.")
+    args = parser.parse_args()
+
+    # Load tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(
+        args.model_path, trust_remote_code=True, padding_side='left'
+    )
+
+    if tokenizer.pad_token_id is None:
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+    if tokenizer.bos_token_id is None:
+        tokenizer.bos_token_id = tokenizer.eos_token_id
+
+    # Load model with bnb_config
+    bnb_config = get_bnb_config()
+    model = AutoModelForCausalLM.from_pretrained(
+        args.model_path, quantization_config=bnb_config, device_map=None, trust_remote_code=True, use_cache=False
+    )
+
+    # Load LoRA adapter
+    if args.adapter_checkpoint_path:
+        model = PeftModel.from_pretrained(model, args.adapter_checkpoint_path)
+        print(f"Load LoRA adapter from {args.adapter_checkpoint_path}")
+    model.eval()
+
+    # Load test data
+    with open(args.test_data_path, "r", encoding="utf-8") as f:
+        test_data = json.load(f)
+
+    results = []
+    print(f"Starting inference with batch size {args.batch_size}...")
+
+    # Generate
+    for i in tqdm(range(0, len(test_data), args.batch_size), desc="Inference"):
+        batch_data = test_data[i: i + args.batch_size]
+        prompts = []
+        for sample in batch_data:
+            prompt = get_prompt_with_template(sample['instruction'], tokenizer)
+            prompts.append(prompt)
+
+        inputs = (tokenizer(
+            prompts,
+            return_tensors="pt",
+            padding=True,
+            truncation=True,
+            max_length=4096
+        ).to(model.device))
+
+        with torch.no_grad():
+            output = model.generate(
+                **inputs,
+                max_new_tokens=256,
+                temperature=0.7,
+                top_p=0.9,
+                do_sample=True,
+                pad_token_id=tokenizer.eos_token_id,
+            )
+
+        decoded_outputs = tokenizer.batch_decode(
+            output[:, inputs.input_ids.shape[1]:], skip_special_tokens=True
+        )
+
+        for j, output in enumerate(decoded_outputs):
+            results.append(
+                {"id": batch_data[j]['id'], "output": output.strip()}
+            )
+
+        if not os.path.exists(os.path.dirname(args.output_path)) and os.path.dirname(args.output_path) != '':
+            os.makedirs(os.path.dirname(args.output_path), exist_ok=True)
+
+        with open(args.output_path, 'w', encoding='utf-8') as f:
+            json.dump(results, f, ensure_ascii=False, indent=2)
+        print(f"Done. Results saved to {args.output_path}")
+
+    if __name__ == "__main__":
+        main()
